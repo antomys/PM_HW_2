c8bb84f4ac1401661e1d8d16efa1b802da9fcc98
antomys <deqque13@gmail.com>
2020-12-18 05:38:22 +0200

    To do 4.2

---
+++ Task_4.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Library;
+
+namespace Task_4._2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                Exceptions();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void Exceptions()
+        {
+            PaymentService paymentService = new PaymentService();
+            Console.WriteLine("Please enter currency. Available: UAH,USD,EUR");
+            var currency = Console.ReadLine().ToUpper();
+            if (currency != "USD" && currency != "EUR" && currency != "UAH")
+            {
+                Console.WriteLine("Try again.");
+                Exceptions();
+            }
+            Console.WriteLine("Please enter amount");
+            decimal amount = 0m;
+            Decimal.TryParse(Console.ReadLine(), out amount);
+            paymentService.StartDeposit(amount,currency);
+        }
+    }
+}
